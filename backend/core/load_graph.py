58cffa89fc5edb19159f1c76879e1acfe87157b1
thomasfitzpa <149018951+thomasfitzpa@users.noreply.github.com>
2025-04-19 13:36:49 -0400

    Update load_graph.py
    moved around formatting for easier to read and made small fixes

--- backend/core/load_graph.py
+++ backend/core/load_graph.py
@@ -5,23 +5,23 @@
             # this is an directed edge, source ---> destanation
 
 
-def load_path(path)
+def load_path(path):
 # Load an edge list into the adjacency list
     # load a directed graph, from the edges files
     # Make the adjasency list into a directorny
 
-graph = {} # empty dictionary, will become  adjacency list 
-
-with open(path, 'r') as file: # open files, and closes auto, READ ONLY mode
-    for line in file: # read 1 at a time
-        two_users = line.strip().split()  # gets rid of whitespace, and splits the edges files into to parts [source, destination]
-    if len(two_users) != 2: # if there isnt 2 users skip it
+    graph = {} # empty dictionary, will become  adjacency list 
+    
+    with open(path, 'r') as file: # open files, and closes auto, READ ONLY mode
+        for line in file: # read 1 at a time
+            two_users = line.strip().split()  # gets rid of whitespace, and splits the edges files into to parts [source, destination]
+            if len(two_users) != 2: # if there isnt 2 users skip it
                 continue
-        src, dst = two_users # source, destination
-        if src not in graph: #if its the first time we see src, add to graph and start edge list
+            src, dst = two_users # source, destination
+            if src not in graph: #if its the first time we see src, add to graph and start edge list
                 graph[src] = []
             graph[src].append(dst) # our directed edge
     return graph #after reading the whole file, it makes the final adjeceny list
-
-#now me and thomas can load edges files like
-    #graph = load_path("backend/data/12831.edges")
+    
+    #now me and thomas can load edges files like
+        #graph = load_path("backend/data/12831.edges")
