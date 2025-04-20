11d11010d1283c3307524a4dfab7b4daf1ea5285
Eric Zhang <superezzhang@gmail.com>
2025-04-19 22:30:41 -0400

    add comments to frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <main className="flex flex-col mx-auto items-center p-4 gap-8">
       <h1 className="text-3xl">Placeholder Title</h1>
+
+      {/* User Input Boxes to Enter User ID's */}
       <div className="flex flex-row gap-8">
         <div className="flex flex-col items items-center">
           <p className="pb-2">User 1</p> 
@@ -13,10 +15,15 @@ export default function Home() {
           <input className="bg-white text-black"></input>
         </div>
       </div>
+
+      {/* Compare */}
       <button className="transform bg-slate-800 text-white text-xl
           py-3 px-4 mr-4 rounded-lg shadow-md 
           transition hover:shadow-xl hover:bg-emerald-500 hover:scale-105 cursor-pointer
           ">Compare</button>
+
+      {/* Results */}
+      
     </main>
   );
 }
