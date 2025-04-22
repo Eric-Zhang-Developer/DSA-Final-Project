a934f418ae62faaf5c515d137f0bcbf07d04bd5a
Sayhan Khan <166950774+sayhan-Khan@users.noreply.github.com>
2025-04-22 12:36:53 -0400

    Update output_dijkstra.py

--- backend/core/output_dijkstra.py
+++ backend/core/output_dijkstra.py
@@ -2,16 +2,19 @@
 #TOOLS
 from core.load_graph import load_path
 from core.Dijkstra import run_dijkstra
-import json #front end
+import json
+import os  # create folders if missing, used for local run
 
-#load graph
-graph = load_path("backend/data/twitter_combined.txt")
-# test case
+#had to do this for local run
+os.makedirs("data/results", exist_ok=True)
+# Load graph
+graph = load_path("data/twitter_combined.txt")
+# Test case
 start = "18687"
 end = "40263"
-# run
+# Run algorithm
 result = run_dijkstra(graph, start, end)
-# Save the result
-with open(f"backend/data/results/dijkstra_combined_{start}_{end}.json", "w") as f:
+# Save result
+with open(f"data/results/dijkstra_combined_{start}_{end}.json", "w") as f:
     json.dump(result, f, indent=2)
 print("Dijkstra output saved.")
