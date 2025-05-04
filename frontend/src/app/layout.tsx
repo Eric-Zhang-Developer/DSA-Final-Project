9a45135df4c22d3561de13eee3851eb584121b33
Eric Zhang <superezzhang@gmail.com>
2025-05-03 23:42:03 -0400

    switch favicon with new logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "TwitterTraverse",
   description: "It's a small world",
+  icons: {
+    icon: '/favicon.png'
+  }
 };
 
 export default function RootLayout({
