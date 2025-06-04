36841774795bc39535d9fb58abe782ad0d80c672
Eric Zhang <superezzhang@gmail.com>
2025-06-04 19:15:04 -0400

    add more connection pairs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,9 +18,32 @@ export default function Home() {
     {user1: "17116707", user2: "28465635"},
     {user1: "221036078", user2: "153460275"},
     {user1: "107830991", user2: "17868918"},
+    
     // Two steps
     {user1: "222261763", user2: "222411742"},
     {user1: "88097807", user2: "109740608"},
+    {user1: "254839786", user2: "35359596"},
+    {user1: "74107696", user2: "221036078"},
+    {user1: "400689940", user2: "131613362"},
+    {user1: "133055665", user2: "187773078"},
+
+    // Three steps
+    {user1: "17759158", user2: "355823615"},
+    {user1: "18996905", user2: "8163442"},
+    {user1: "34428380", user2: "260769396"},
+    {user1: "394263193", user2: "176222605"},
+    {user1: "394263193", user2: "26649453"},
+
+    // Four steps
+    {user1: "21146135", user2: "404376053"},
+
+    // Five steps 
+    {user1: "283306479", user2: "14050306"},
+
+    // Invalid 
+    {user1: "2704495328", user2: "8163442"},
+    {user1: "2704498", user2: "2163442"},
+
   ]
 
   // State mangement, variables live here
@@ -61,7 +84,8 @@ export default function Home() {
     try {
       // call backend
       // localhost for now
-      const response = await fetch("https://cop3530-final-project-twitter-traverse.onrender.com/api/compare", {
+      // https://cop3530-final-project-twitter-traverse.onrender.com/api/compare
+      const response = await fetch("http://127.0.0.1:5001/api/compare", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ start: user1, end: user2 }),
